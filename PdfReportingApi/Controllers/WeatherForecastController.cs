8075f63bf2fac77c8851d7d7c18aae1df65ce45a
Mehdi Rifi <mehdirifi51@gmail.com>
2019-12-30 10:23:50 +0100

    Add project files.

---
+++ PdfReportingApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfReporting;
+using PdfReportingApi;
+using PdfReportingApi.Views;
+
+namespace PdfReportingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IViewRender _viewRender;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+
+        public WeatherForecastController(IViewRender viewRender,IWebHostEnvironment hostingEnvironment)
+        {
+            _viewRender = viewRender;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get()
+        {
+            string html = await _viewRender.RenderAsync("khedama", new TDto { Name="Mehdi Rifi"});
+            return Ok();
+        }
+    }
+}
---
+++ PdfReportingApi/TDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfReporting
+{
+    public class TDto
+    {
+        public string Name { get; set; }
+    }
+}
