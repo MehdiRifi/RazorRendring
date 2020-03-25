9ea6e9262575cbd4f75579de88b3baea65165c90
Mehdi Rifi <mehdirifi51@gmail.com>
2020-03-25 20:01:27 +0100

    refactor: move IViewRender in a separate file.

---
+++ RazorRendering/IViewRender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RazorRendering
+{
+    public interface IViewRender
+    {
+        /// <summary>
+        /// Render Razor file.
+        /// </summary>
+        /// <param name="name"> name of the razor file</param>
+        /// <returns></returns>
+        Task<string> RenderAsync(string name);
+
+        /// <summary>
+        /// Render Razor file.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="name"> name of the razor file</param>
+        /// <param name="model"> the object to be passed to the razor file</param>
+        /// <returns></returns>
+        Task<string> RenderAsync<TModel>(string name, TModel model);
+    }
+}
